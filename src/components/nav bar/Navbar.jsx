18b717aec02d3d5ec7075f8189684eb2208cc7ea
obukhwo oviemo <CODE.x.@MACs-MacBook-Air-2.local>
2022-09-02 17:26:13 +0100

    navbar and login

---
+++ src/components/nav bar/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div className="flex flex-row w-full p-3 justify-around text-center items-center absolute bg-blue-300">
+      <h1>ICON</h1>
+      <div>
+        <ul className="flex flex-row">
+          <Link to="/home">
+            <li className="mx-4">Home</li>
+          </Link>
+          <Link to="/home">
+            <li className="mx-4">Courses</li>
+          </Link>
+          <Link to="/home">
+            <li className="mx-4">About</li>
+          </Link>
+        </ul>
+      </div>
+      <div>
+        <Link to="/login">
+          <button className="mx-2 p-2 rounded-md bg-blue-600 text-white">
+            Login
+          </button>
+        </Link>
+        <Link to="/home">
+          <button className="mx-2 p-2 rounded-md bg-blue-600 text-white">
+            Sign up
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
