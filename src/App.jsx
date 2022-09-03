5f9e9e56de51c91319076f7608c46d1e252f489b
Opubor Tony <opubortony@gmail.com>
2022-09-03 10:31:41 -0700

    adminSidebar and forgotPassword

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
 import Blogs from './components/blogs/Blogs'
+import AdminSidebar from './components/adminSidebar/AdminSidebar.jsx'
+import ForgotPassword from './components/forgotPassword/ForgotPassword.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <Blogs/>
+      <AdminSidebar/>
+      <ForgotPassword/>
     </div>
   )
 }
