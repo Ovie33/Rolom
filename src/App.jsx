95187b9a09c9a7d9d962b1d198d4eac9a2c1a3b0
freemann firms <freemannfirm@gmail.com>
2022-09-03 11:23:16 +0100

    I added the blog list

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
-import Blogs from './components/blogs/Blogs'
+import Bloglist from './pages/bloglist/Bloglist'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <Blogs/>
+      <Bloglist/>
     </div>
   )
 }
