0e4a5908a13283a4571fa6a39eaaef2a91af3c6c
OGHENEMINE <66541923+OGHENEMINE@users.noreply.github.com>
2022-09-03 10:35:43 +0100

    gitignore added

--- src/components/studentSidebar/StudentSidebar.jsx
+++ src/components/studentSidebar/StudentSidebar.jsx
@@ -6,6 +6,7 @@ import examinationLogo from '../studentSidebar/examination.svg'
 import blogLogo from '../studentSidebar/brand-blogger.svg'
 import resultLogo from '../studentSidebar/result.svg'
 import AttendanceLogo from '../studentSidebar/list-check.svg'
+import { Link } from "react-router-dom";
 
 function StudentSidebar() {
   return (
@@ -14,12 +15,24 @@ function StudentSidebar() {
         <h4 className='mt-5'>LOGO</h4>
         <p className='text-zinc-400 mt-10'>MENU</p>
         <div className='flex flex-col pl-3 pt-4'>
+              <Link to="/home">
               <StudentNavItem icon={dashboardLogo} label='Dashboard'/>
+              </Link>
+              <Link to="/home">
               <StudentNavItem icon={coursesLogo} label='Courses'/>
+              </Link>
+              <Link to="/home">
               <StudentNavItem icon={blogLogo} label='Blog'/>
+              </Link>
+              <Link to="/home">
               <StudentNavItem icon={examinationLogo} label='Examination'/>
+              </Link>
+              <Link to="/home">
               <StudentNavItem icon={resultLogo} label='Results'/>
+              </Link>
+              <Link to="/home">
               <StudentNavItem icon={AttendanceLogo} label='Attendance'/>
+              </Link>
         </div>
     </div>
     </div>
