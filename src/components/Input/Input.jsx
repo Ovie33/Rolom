fd9cab6146c1f4a3c3ee63aee64591c0c2ebc690
OGHENEMINE <66541923+OGHENEMINE@users.noreply.github.com>
2022-09-04 03:40:58 +0100

    still updating

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 function Input( {type, name, placeholder, id} ) {
   return (
     <div>
        <input type={type} name={name} id={id} placeholder={placeholder} 
 =======
+=======
+>>>>>>> 8bd0b28ecaeb97768a6775cb91633890f88f156e
 function Input( {name, placeholder, id} ) {
   return (
     <div>
        <input type="text" name={name} id={id} placeholder={placeholder} 
+<<<<<<< HEAD
+>>>>>>> 8bd0b28ecaeb97768a6775cb91633890f88f156e
+=======
 >>>>>>> 8bd0b28ecaeb97768a6775cb91633890f88f156e
        className='block p-1.5 mb-5 bg-zinc-50 border-b-2 text-sm focus:outline-none w-80 border-b-blue-400'/>
     </div>
