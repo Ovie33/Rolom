89b7611cb46ba6eff3324f12173e9c7032faf45a
obukhwo oviemo <CODE.x.@MACs-MacBook-Air-2.local>
2022-09-05 20:19:50 +0100

    made some minor things

--- src/components/studentSidebar/StudentSidebar.jsx
+++ src/components/studentSidebar/StudentSidebar.jsx
@@ -1,42 +1,43 @@
-import React from 'react';
-import StudentNavItem from '../StudentNavItem/StudentNavItem'
-import dashboardLogo from '../studentSidebar/dashboard.svg'
-import coursesLogo from '../studentSidebar/courses.svg'
-import examinationLogo from '../studentSidebar/examination.svg'
-import blogLogo from '../studentSidebar/brand-blogger.svg'
-import resultLogo from '../studentSidebar/result.svg'
-import AttendanceLogo from '../studentSidebar/list-check.svg'
+import React from "react";
+import dashboardLogo from "./assets/dashboard.svg";
+import coursesLogo from "./assets/courses.svg";
+import examinationLogo from "./assets/examination.svg";
+import blogLogo from "./assets/brand-blogger.svg";
+import resultLogo from "./assets/result.svg";
+import AttendanceLogo from "./assets/list-check.svg";
 import { Link } from "react-router-dom";
+import NavItem from "../navItem/NavItem";
 
 function StudentSidebar() {
   return (
     <div>
-      <div className='bg-blue-200 w-40 h-screen p-5'>
-        <h4 className='mt-5'>LOGO</h4>
-        <p className='text-zinc-400 mt-10'>MENU</p>
-        <div className='flex flex-col pl-3 pt-4'>
-              <Link to="/home">
-              <StudentNavItem icon={dashboardLogo} label='Dashboard'/>
-              </Link>
-              <Link to="/home">
-              <StudentNavItem icon={coursesLogo} label='Courses'/>
-              </Link>
-              <Link to="/home">
-              <StudentNavItem icon={blogLogo} label='Blog'/>
-              </Link>
-              <Link to="/home">
-              <StudentNavItem icon={examinationLogo} label='Examination'/>
-              </Link>
-              <Link to="/home">
-              <StudentNavItem icon={resultLogo} label='Results'/>
-              </Link>
-              <Link to="/home">
-              <StudentNavItem icon={AttendanceLogo} label='Attendance'/>
-              </Link>
+      <div className="flex p-9 h-screen flex-col w-64  bg-blue-200 opacity-40 shadow-slate-700 shadow-md ">
+        <h4>LOGO</h4>
+
+        <div className="h-1/2 flex mt-24 flex-col justify-between">
+          <h1 className="text-black text-xl mb-3 font-bold">Menu</h1>
+          <Link to="/home">
+            <NavItem icon={dashboardLogo} label="Dashboard" />
+          </Link>
+          <Link to="/home">
+            <NavItem icon={coursesLogo} label="Courses" />
+          </Link>
+          <Link to="/home">
+            <NavItem icon={blogLogo} label="Blog" />
+          </Link>
+          <Link to="/home">
+            <NavItem icon={examinationLogo} label="Examination" />
+          </Link>
+          <Link to="/home">
+            <NavItem icon={resultLogo} label="Results" />
+          </Link>
+          <Link to="/home">
+            <NavItem icon={AttendanceLogo} label="Attendance" />
+          </Link>
         </div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default StudentSidebar
+export default StudentSidebar;
